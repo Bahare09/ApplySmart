0ebcb9f4a4cc960c3a898de4be7c9c17e70dbacd
BoshraM <gbboshra@gmail.com>
2023-10-13 13:42:47 +0100

    creating functiom to read pdf

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
+const fs = require("fs");
+const pdf = require("pdf-parse");
 const app = express();
 const port = 3100;
 
@@ -9,13 +11,8 @@ const upload = multer({ storage: storage });
 
 app.use(express.json());
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
 app.post("/upload-cv", upload.single("cv"), (req, res) => {
-  const cvFile = req.file; // Access the uploaded CV file
+  const cvFile = req.file;
 
   if (!cvFile) {
     return res.status(400).json({ error: "No CV file provided" });
@@ -24,6 +21,19 @@ app.post("/upload-cv", upload.single("cv"), (req, res) => {
   return res.json({ message: "CV uploaded successfully!" });
 });
 
+async function readCvFileContent(cvFile) {
+  try {
+    const dataBuffer = fs.readFileSync(cvFile.path);
+    const dataString = dataBuffer.toString();
+    const pdfData = await pdf(dataString);
+    const pdfText = pdfData.text;
+    console.log(pdfText)
+    return pdfText;
+  } catch (error) {
+    console.error("Error reading PDF file:", error);
+    throw error;
+  }
+}
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
