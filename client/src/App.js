16ad79ac5a8a039a8407b63fdb079319fab9796a
Bahare <itxbb09@gmail.com>
2023-10-13 20:38:41 +0100

    change uploadfile.js for choosing file type

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,31 @@ import SubmitText from "./components/SubmitText";
 import UploadFile from "./components/UploadFile ";
 
 function App() {
+  const handleFileUpload = async (file) => {
+    if (file) {
+      try {
+        // Send a POST request to the backend with the CV file
+        const formData = new FormData();
+        formData.append("cv", file);
+
+        const response = await fetch("http://localhost:4000/upload-cv ", {
+          method: "POST",
+          body: formData,
+        });
+
+        // Check if the response is successful
+        if (response.ok) {
+          alert("CV uploaded successfully!");
+        } else {
+          alert("CV upload failed.");
+        }
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    } else {
+      alert("Please select a CV file to upload.");
+    }
+  };
   const handleTextSubmit = async (text) => {
     if (text) {
       try {
@@ -30,8 +55,9 @@ function App() {
 
   return (
     <div className="App">
-      <UploadFile />
-      <SubmitText onTextSubmit={handleTextSubmit} />
+      <UploadFile onFileUpload={handleFileUpload} fileType="cv" />
+      <SubmitText onTextSubmit={handleTextSubmit} fileType="cv" />
+      <UploadFile onFileUpload={handleFileUpload} fileType="job" />
     </div>
   );
 }
--- client/src/components/UploadFile .js
+++ client/src/components/UploadFile .js
@@ -1,57 +1,26 @@
 import React, { useState } from "react";
 
-function UploadFile() {
-  // State to hold the selected CV file
-  const [selectedCV, setSelectedCV] = useState(null);
+function UploadFile({ onFileUpload, fileType }) {
+  const [selectedFile, setSelectedFile] = useState(null);
 
-  // Function to handle CV file upload
-  const handleCVUpload = async () => {
-    // Check if a CV file is selected
-    if (!selectedCV) {
-      alert("Please select a CV file to upload.");
-      return;
-    }
-
-    // Create a FormData object to send the CV file
-    const formData = new FormData();
-    formData.append("cv", selectedCV);
-
-    try {
-      // Send a POST request to the backend with the CV file
-      const response = await fetch("http://localhost:4000/upload-cv ", {
-        method: "POST",
-        body: formData,
-      });
-
-      // Check if the response is successful
-      if (response.ok) {
-        alert("CV uploaded successfully!");
-        // Clear the selectedCV state after successful upload
-        setSelectedCV();
-      } else {
-        alert("CV upload failed.");
-      }
-    } catch (error) {
-      console.error("An error occurred:", error);
-    }
-  };
-
-  // Function to handle changes in the CV file input
-  const handleCVChange = (e) => {
+  const handleFileChange = (e) => {
     const file = e.target.files[0];
-    setSelectedCV(file);
+    setSelectedFile(file);
   };
 
-  // Render the component's UI
+  const handleFileUpload = () => {
+    if (selectedFile) {
+      onFileUpload(selectedFile, fileType); // Pass the selected file and type
+    } else {
+      alert("Please select a file to upload.");
+    }
+  };
+
   return (
     <div>
-      <h3>Upload your CV (PDF)</h3>
-      <input
-        type="file"
-        accept=".pdf" // Allow only PDF files to be selected
-        onChange={handleCVChange} // Call handleCVChange when the file input changes
-      />
-      <button onClick={handleCVUpload}>Upload</button>
+      <h3>Upload {fileType === "cv" ? "CV" : "Job Description"} (PDF)</h3>
+      <input type="file" accept=".pdf" onChange={handleFileChange} />
+      <button onClick={handleFileUpload}>Upload</button>
     </div>
   );
 }
