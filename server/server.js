cd345bc774e741a8ae69ca0c5acdca93f19b854d
BoshraM <gbboshra@gmail.com>
2023-10-17 13:39:35 +0100

    add function to tailer new cv

--- server/server.js
+++ server/server.js
@@ -46,6 +46,29 @@ async function extractSkillsFromCV(cvText) {
   }
 }
 
+async function tailorCv(cvText, jobText) {
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `take the following CV and Job description then create a new cv depends on job description. cv: ${cvText}  job description :${jobText}`,
+        },
+      ],
+    });
+
+    if (!response || !response.choices || response.choices.length === 0) {
+      throw new Error("API response is invalid");
+    }
+    const tailoredCv = response.choices[0].message.content.trim();
+    return tailoredCv;
+  } catch (error) {
+    console.error("Error in tailorCvAndCreateCoverLetter:", error);
+    res.status(500).json({ error: "An error occurred while processing the request." });
+  }
+}
+
 async function readPdfFileContent(file) {
   try {
     const buffer = file.buffer;
