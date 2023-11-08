a56e11360140375802b37ad59fae5633bde43340
BoshraM <gbboshra@gmail.com>
2023-11-08 01:55:10 +0000

    move uploadCv  function to its file

---
+++ server/functions/uploadCv.js
@@ -0,0 +1,54 @@
+const { readPdfFileContent } = require("./pdf");
+const { extractSkillsFromCV } = require("./openai")
+const uploadCv = async (req, res, db) => {
+    const { file } = req;
+    const fileType = req.body.type; // Access the type field (cv or job)
+
+    if (!file) {
+        return res.status(400).json({ error: "No file provided" });
+    }
+
+    try {
+        // Determine if it's a CV or job description based on the fileType
+        if (fileType === "cv") {
+            // Handle CV processing
+            const cvText = await readPdfFileContent(file);
+            const skills = await extractSkillsFromCV(cvText); // Extract skills from the CV
+
+            // Insert CV into the database and store the skills
+            const cvInsertQuery =
+                "INSERT INTO cv (cv_text, skill_words) VALUES ($1, $2) RETURNING cv_id";
+            const cvInsertResult = await db.query(cvInsertQuery, [cvText, skills]);
+            cvId = cvInsertResult.rows[0].cv_id;
+
+            return res.json({
+                message: "CV uploaded and processed successfully.",
+                text: cvText,
+            });
+        } else if (fileType === "job") {
+            // Handle job description processing
+            const jobText = await readPdfFileContent(file);
+
+            // Insert job description into the database
+            const jobInsertQuery =
+                "INSERT INTO job_description (job_text, cv_id) VALUES ($1, $2)";
+            await db.query(jobInsertQuery, [jobText, cvId]);
+
+            return res.json({
+                message: "Job description uploaded and processed successfully.",
+                text: jobText,
+            });
+        } else {
+            return res.status(400).json({ error: "Invalid file type" });
+        }
+    } catch (error) {
+        console.error("Error processing file:", error);
+        res
+            .status(500)
+            .json({ error: "An error occurred while processing the request." });
+    }
+}
+
+module.exports = {
+    uploadCv,
+};
