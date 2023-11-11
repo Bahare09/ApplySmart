a5e834f8a87ca30b6f1d5986d255d6d9120054fa
Bahare <itxbb09@gmail.com>
2023-11-11 23:13:35 +0000

    Adding useEffect to generate the job list when entering the JobListingPage

--- client/src/pages/JobListingPage.js
+++ client/src/pages/JobListingPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import UploadFile from "../components/UploadFile";
 import SubmitText from "../components/SubmitText";
@@ -28,6 +28,9 @@ function JobListingPage({
         return null;
     }
   };
+  useEffect(() => {
+    generateJobList();
+  }, [generateJobList]);
 
   return (
     <div>
