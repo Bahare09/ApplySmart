37d12a5b8320b8ecf49a753a5e3b66e00aafdb9c
Bahare <itxbb09@gmail.com>
2023-11-15 21:49:34 +0000

    create  sendJobDForView endpoint to send the fulljobD to frontend

---
+++ server/functions/sendJobDForView.js
@@ -0,0 +1,22 @@
+const {
+  extractJobDescriptionFromURL,
+} = require("./extractJobDescriptionFromURL");
+const sendJobDForView = async (req, res, db) => {
+  const { Url } = req.body;
+  console.log(Url);
+  // Check if Url is provided
+  if (!Url) {
+    return res
+      .status(400)
+      .json({ error: "Url is required in the request body" });
+  }
+  try {
+    const fullJobDescription = await extractJobDescriptionFromURL(Url);
+    console.log(fullJobDescription);
+    res.json({ fullJobDescription });
+  } catch (error) {
+    console.error("Error extracting job description:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+module.exports = { sendJobDForView };
