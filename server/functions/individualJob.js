9e85d26721b6716d9c16905a17185e16198faa96
Bahare <itxbb09@gmail.com>
2023-11-15 11:07:50 +0000

    update individualJob.js

--- server/functions/individualJob.js
+++ server/functions/individualJob.js
@@ -1,13 +1,31 @@
+const {
+  extractJobDescriptionFromURL,
+} = require("./extractJobDescriptionFromURL");
+
 const { processDataForJob } = require("./processDataForJob");
 
-
 const individualJob = async (req, res, db) => {
-    const { description } = req.body;
+  try {
+    const { Url } = req.body;
 
-    // Pass the description to the processDataForJob function
-    await processDataForJob(cvId, description, db, res);
+    // Check if Url is provided
+    if (!Url) {
+      return res
+        .status(400)
+        .json({ error: "Url is required in the request body" });
+    }
+
+    // Extract job description from the provided URL
+    const Description = await extractJobDescriptionFromURL(Url);
+
+    // Pass the extracted job description to the processDataForJob function
+    await processDataForJob(cvId, Description, db, res);
+  } catch (error) {
+    console.error("An error occurred:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 module.exports = {
-    individualJob,
+  individualJob,
 };
