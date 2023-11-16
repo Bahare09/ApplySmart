ddbcbed14071b54412effd4dc3d6918a3d8b55da
BoshraM <gbboshra@gmail.com>
2023-11-16 01:35:13 +0000

    split job description before sending to front end

--- server/functions/extractJobDescriptionFromURL.js
+++ server/functions/extractJobDescriptionFromURL.js
@@ -1,20 +1,24 @@
 const cheerio = require("cheerio");
+
 const extractJobDescriptionFromURL = async (redirectUrl) => {
   try {
     const response = await fetch(redirectUrl);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch job page. Status: ${response.status}`);
+    const extractJobDescriptionFromURL = async (redirectUrl) => {
     }
     const html = await response.text();
     const $ = cheerio.load(html);
-    const fullJobDescriptionText = $("section.adp-body").text();
-    console.log("Full Job Description Text:", fullJobDescriptionText);
+
+    const fullJobDescriptionText = $("section.adp-body").text().split('\n\n');
+
+    // console.log("Full Job Description Text:", fullJobDescriptionText);
+
     return fullJobDescriptionText;
   } catch (error) {
     console.error("Error extracting job description:", error.message);
     return null;
   }
 };
+
 module.exports = {
   extractJobDescriptionFromURL,
 };
