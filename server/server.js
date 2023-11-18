75d5e8c48b4f7f4cd97ec7b95d79e7fae45b021b
Bahare <itxbb09@gmail.com>
2023-11-18 20:00:09 +0000

    Manage the CORS for different environments

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const { sendJobDForView } = require("./functions/sendJobDForView");
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://applysmartc.onrender.com",
+    origin: ["http://localhost:3000", "https://applysmartc.onrender.com"],
   })
 );
 app.use(bodyParser.json());
