8d00c387a2156cc7e6fca31aff7018dfc1e3fb5b
BoshraM <gbboshra@gmail.com>
2023-11-20 12:50:48 +0000

    applying circle loading on home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,46 +1,32 @@
 import React, { useState } from "react";
 import SubmitText from "../components/SubmitText";
 import UploadFile from "../components/UploadFile";
+import "./jobListingpage.css"
 
-function HomePage({ handleFileUpload, handleTextSubmit }) {
+function HomePage({ handleFileUpload, handleTextSubmit, loading }) {
   const [selectedOption, setSelectedOption] = useState(null);
-  const [isUploading, setIsUploading] = useState(false);
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
 
-  const handleFileUploadWrapper = async (file, fileType) => {
-    setIsUploading(true); // Set uploading to true before starting upload
-    await handleFileUpload(file, fileType);
-    setIsUploading(false); // Set uploading to false after upload completion
-  };
-
-  const handleTextSubmitWrapper = async (text, fileType) => {
-    setIsUploading(true); // Set uploading to true before starting text submit
-    await handleTextSubmit(text, fileType);
-    setIsUploading(false); // Set uploading to false after text submit completion
-  };
-
   return (
     <div>
-      <select value={selectedOption} onChange={handleOptionChange}>
-        <option value="">Select an option</option>
-        <option value="text">Submit Text</option>
-        <option value="file">Upload File</option>
-      </select>
-
-      {isUploading && <p>Loading...</p>}
-
-      {selectedOption === "text" && !isUploading && (
-        <SubmitText onTextSubmit={handleTextSubmitWrapper} fileType="cv" />
-      )}
-
-      {selectedOption === "file" && !isUploading && (
-        <UploadFile onFileUpload={handleFileUploadWrapper} fileType="cv" />
-      )}
+      <div>
+        <select value={selectedOption} onChange={handleOptionChange}>
+          <option value="">Select an option</option>
+          <option value="text">Submit Text</option>
+          <option value="file">Upload File</option>
+        </select>
+        {selectedOption === "text" && (
+          <SubmitText onTextSubmit={handleTextSubmit} fileType="cv" />
+        )}
+        {selectedOption === "file" && (
+          <UploadFile onFileUpload={handleFileUpload} fileType="cv" />
+        )}
+      </div>
+      {loading ? <div className="loading"><p>Loading...</p></div> : ""}
     </div>
   );
 }
-
 export default HomePage;
