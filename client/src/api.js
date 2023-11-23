c2a92829aef1167f2a2a97295c593ae39fc951e3
Bahare <itxbb09@gmail.com>
2023-11-23 15:17:02 +0000

    updat for CORS and server link in production and development environment

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,7 @@
-const apiUrl =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_URL_PROD
-    : process.env.REACT_APP_API_URL_DEV;
+// const apiUrl =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REACT_APP_API_URL_PROD
+//     : process.env.REACT_APP_API_URL_DEV;
 
-export default apiUrl;
+// export default apiUrl;
+export default process.env.REACT_APP_API_URL;
