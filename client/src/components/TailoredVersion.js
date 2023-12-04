c7ee42d7e43ce94a0b2c81f663f06756b4304d13
BoshraM <gbboshra@gmail.com>
2023-12-04 00:00:09 +0000

    importing DownloadCopySection

--- client/src/components/TailoredVersion.js
+++ client/src/components/TailoredVersion.js
@@ -1,34 +1,44 @@
-import { Tabs, Flex, Typography } from "antd"
-const { Title } = Typography
+import React from "react";
+import { Tabs, Flex, Typography } from "antd";
+import DownloadCopySection from "./DownloadCopySection";
+
+const { Title } = Typography;
+
 export const TailoredVersion = ({ cv, coverLetter }) => {
-    console.log(cv)
-
-    return <Flex vertical>
-        <Title level={4}>
-            The tailor CV and cover letter
-        </Title>
-
-        <Tabs
-            defaultActiveKey="1"
-            type="card"
-            size={"middle"}
-            items={[{
-                label: "CV",
-                key: "1",
-                children: cv.split('\n\n').map((paragraph, index) => <p key={index}>{paragraph}</p>)
-            }
-                ,
-            {
-                label: "Cover letter",
-                key: "2",
-                children: coverLetter.split('\n\n').map((paragraph, index) => <p key={index}>{paragraph}</p>)
-
-            }]
-            }
-        />
-
-
-
-    </Flex>
-
-}
+	return (
+		<Flex style={{ flex: "1" }} vertical>
+			<Title level={4}>The Tailor CV and Cover Letter</Title>
+			<Tabs
+				defaultActiveKey="1"
+				type="card"
+				size={"middle"}
+				items={[
+					{
+						label: "CV",
+						Title: "CV",
+						key: "1",
+						children: (
+							<DownloadCopySection
+								text={cv}
+								fileName="cv.txt"
+								title="CV"
+							/>
+						)
+					},
+					{
+						label: "Cover letter",
+						Title: "Cover letter",
+						key: "2",
+						children: (
+							<DownloadCopySection
+								text={coverLetter}
+								fileName="coverLetter.txt"
+								title="Cover Letter"
+							/>
+						)
+					}
+				]}
+			/>
+		</Flex>
+	);
+};
