e3706c20b69c612e4641a92ada11a7f9127d137a
cyf <gbboshra@gmail.com>
2024-01-18 23:45:56 +0000

    make header responsive

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -1,20 +1,22 @@
-import { Flex, Layout, Typography, Image } from "antd";
-import Graphic from "../components/images/Graphic-header.svg"
+import { Flex, Layout, Typography, Image, Grid } from "antd";
+import Graphic from "../components/images/Graphic-header.svg";
 const { Title } = Typography;
-const { Header } = Layout
-
+const { Header } = Layout;
+const { useBreakpoint } = Grid;
 
 const AppHeader = () => {
-
+    const breakpoints = useBreakpoint();
     return (
         <Header style={{ backgroundColor: "rgba(250, 250, 250, 1)" }} >
-            <Flex align="baseline" justify="left" gap="24px">
-                <Title level={4}>ApplySmart</Title>
+            <Flex align="baseline"  justify= "left" >
+                <Title level={1}>ApplySmart</Title>
+              {!breakpoints.xs &&
                 <Image
                     width={"94px"}
                     height={"66px"}
                     src={Graphic}
-                />
+                    // style={{display: breakpoints.xs ? "none" : "block"}}
+                />}
             </Flex>
         </Header>
     )
